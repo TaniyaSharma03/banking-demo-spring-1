e16f45862182f4f9a8c97684d0dfdb38c015b354
Ashish <juyal.ashish@gmail.com>
2022-02-22 12:09:12 +0530

    added qualifier

--- src/main/java/org/example/repository/CustomerRepositoryDB.java
+++ src/main/java/org/example/repository/CustomerRepositoryDB.java
@@ -7,7 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
+@Repository("db")
 public class CustomerRepositoryDB implements CustomerRepository {
 
   public List<Customer> findAll() {
