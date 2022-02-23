0ac58c1d96f91c6a24a0ab279db95757856af4c5
Ashish <juyal.ashish@gmail.com>
2022-02-23 12:12:12 +0530

    Added spring-boot configuration for web application and configured the controller for using customerService

--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -2,12 +2,16 @@ package org.example.service;
 
 import org.example.model.Customer;
 import org.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service("customerService")
 public class DefaultCustomerService implements CustomerService {
 
+  @Autowired
   private CustomerRepository repository;
 
   public DefaultCustomerService() {}
